b715b0486a594e6dd8b3f3e948d4b9debf933343
taro.kirameki <taro.kirameki@techacademy.jp>
2022-03-21 13:04:51 +0000

    add colum

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_20_050032) do
+ActiveRecord::Schema.define(version: 2022_03_21_115411) do
 
   create_table "tasks", charset: "utf8mb4", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "status"
   end
 
 end
