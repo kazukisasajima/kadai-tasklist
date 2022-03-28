77b4425658e16260b7375629696f70f20246b971
taro.kirameki <taro.kirameki@techacademy.jp>
2022-03-28 10:27:06 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
-  get 'toppages/index'
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
   # For details on the DSL vailable within this file, see https://guides.rubyonrails.org/routing.html
+  
+  get 'toppages/index'
+  
   root to: 'toppages#index'
   # root to: 'tasks#index'
   
+  get 'signup', to: 'users#new'
+  
+  resources :users, only: [:index, :show, :create, :update]
   resources :tasks
 end
