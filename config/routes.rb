75029c84ffd31418bef5a33cfaca88b2d85e71a5
taro.kirameki <taro.kirameki@techacademy.jp>
2022-03-28 09:41:45 +0000

    top page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  get 'toppages/index'
   # For details on the DSL vailable within this file, see https://guides.rubyonrails.org/routing.html
-  
-  root to: 'tasks#index'
+  root to: 'toppages#index'
+  # root to: 'tasks#index'
   
   resources :tasks
 end
